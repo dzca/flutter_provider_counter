63133e0e72a189903bb710c62e53022b708d10be
dzca <dike.zhang@gmail.com>
2024-09-23 15:22:19 -0400

    add bottom tabs

---
+++ lib/widgets/nav_layout.dart
@@ -0,0 +1,44 @@
+import '../screens/home_screen.dart';
+
+import 'package:flutter/material.dart';
+
+import '../screens/settings_screen.dart';
+
+class NavLayout extends StatefulWidget {
+  const NavLayout({super.key});
+
+  @override
+  State<NavLayout> createState() => _NavLayoutState();
+}
+
+class _NavLayoutState extends State<NavLayout> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        /// Replaced with IndexedStack
+        index: _selectedIndex,
+        children: const [
+          HomeScreen(),
+          SettingsScreen(),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onItemTapped,
+      ),
+    );
+  }
+}
